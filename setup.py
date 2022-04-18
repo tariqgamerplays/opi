0ba42b51e26424ff0ff8d2f457ec1898487c78f1
tariqgamerplays <63972504+tariqgamerplays@users.noreply.github.com>
2022-04-18 21:26:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audi
 
 # Setting up
 setup(
-    name="vidstream",
+    name="TING",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Neur",
+    author_email="<mail@neuraline.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=[],
+    keywords=['python'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
